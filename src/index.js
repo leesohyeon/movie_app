27e30e7c3da072b39564ae19343cab11add7601d
leesohyeon <dltdhgus0702@gmail.com>
2019-11-19 09:50:57 +0900

    create mt first component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App /><Potato />, document.getElementById('root'));
+//ㄴ작동하지 않음 , 하나의 component만
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+/*
+<App /> html 처럼 보이지만 html 이 아님
+Component, react는 항상 component와 동작함
+ㄴhtml을 반환하는 함수 -> ***항상 대문자로 시작***
+react는 component를 사용해서 HTML처럼 작성하려는 경우에 필요
+*javascript & html = JSX -react에서 나온 개념 > 오직 react에서만
+*/
+
+
